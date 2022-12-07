777072041cc18fd95f8c81b1be7204fbff1d3d17
Justin R. Wilson <wilsonj@objectcomputing.com>
2022-12-07 15:26:34 -0600

    URL changed

--- Time/common.sh
+++ Time/common.sh
@@ -20,7 +20,7 @@ EOF
     curl --silent -b cookies.txt ${DPM_URL}/api/applications/identity_ca.pem > "${directory}/identity_ca.pem"
     curl --silent -b cookies.txt ${DPM_URL}/api/applications/permissions_ca.pem > "${directory}/permissions_ca.pem"
     curl --silent -b cookies.txt ${DPM_URL}/api/applications/governance.xml.p7s > "${directory}/governance.xml.p7s"
-    curl --silent -b cookies.txt ${DPM_URL}/api/applications/key-pair?nonce=NONCE > "${directory}/key-pair"
+    curl --silent -b cookies.txt ${DPM_URL}/api/applications/key_pair?nonce=NONCE > "${directory}/key-pair"
     jq -r '.public' "${directory}/key-pair" > "${directory}/identity.pem"
     jq -r '.private' "${directory}/key-pair" > "${directory}/identity_key.pem"
     curl --silent -b cookies.txt ${DPM_URL}/api/applications/permissions.xml.p7s?nonce=NONCE > "${directory}/permissions.xml.p7s"
